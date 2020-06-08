934799d3eb6a8a466a90530eb599ce05b9e5e7af
Amar Tyagi <amar.tyagi.cse@gmail.com>
2020-06-07 20:22:24 -0700

    second commit

--- MyNewMaven/src/main/java/com/intuitive/main/MyLogger.java
+++ MyNewMaven/src/main/java/com/intuitive/main/MyLogger.java
@@ -9,6 +9,7 @@ public class MyLogger {
 	public static void main(String[] args) {
 		
 		log.debug("I am in debug logger");
+		log.debug("I am in another debug logger");
 		log.info("I ma in log info");
 
 	}
