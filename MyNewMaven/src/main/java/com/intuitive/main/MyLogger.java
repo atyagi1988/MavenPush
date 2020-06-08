0d7f8695beee7f0dd128e250542db42c73cb8206
Amar Tyagi <amar.tyagi.cse@gmail.com>
2020-06-07 23:34:55 -0700

    another stamp

--- MyNewMaven/src/main/java/com/intuitive/main/MyLogger.java
+++ MyNewMaven/src/main/java/com/intuitive/main/MyLogger.java
@@ -9,7 +9,7 @@ public class MyLogger {
 	public static void main(String[] args) {
 		
 		log.debug("I am in debug logger");
-		log.debug("I am in debug logger 2");
+		log.debug("I am in debug logger 3");
 		log.debug("I am in another debug logger");
 		log.info("I ma in log info");
 
